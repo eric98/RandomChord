88bdd20d59e81f549187af4f019cbd2729922089
Eric <ergare.17@gmail.com>
2021-04-10 02:44:35 +0200

    beats with js

--- main.js
+++ main.js
@@ -12,5 +12,67 @@ var outputBeats = document.getElementById("beatsSpan");
 outputBeats.innerHTML = sliderBeats.value;
 
 sliderBeats.oninput = function() {
-    outputBeats.innerHTML = this.value;
+  outputBeats.innerHTML = this.value;
+}
+
+var imagesDiv = document.getElementById("imagesDiv");
+var lastImage = document.getElementById("lastImage");
+var actualImage = document.getElementById("actualImage");
+var nextImage = document.getElementById("nextImage");
+
+var imageCheck = document.getElementById("_images");
+imageCheck.checked = true;
+
+function enableImages() {
+  let visibility;
+  let display;
+
+  if (imageCheck.checked) {
+    visibility = 'visible';
+    display = "";
+  }
+  else {
+    visibility = 'hidden';
+    display = "none";
+  }
+
+  lastImage.style.visibility = visibility;
+  actualImage.style.visibility = visibility;
+  nextImage.style.visibility = visibility;
+
+  imagesDiv.style.display = display;
+}
+
+const deltaTime = 16.67;
+var lastUpdate = Date.now();
+var myInterval = setInterval(tick, deltaTime);
+
+var timeNow = 0;
+var nextKlack = 0;
+
+var myBeats = document.getElementById("myBeats");
+var beatCount = 0;
+
+function tick() {
+    // var now = Date.now();
+    // var dt = now - lastUpdate;
+    // lastUpdate = now;
+
+    // update(dt);
+    // render(dt);
+
+    timeNow += deltaTime;
+
+    if (timeNow > nextKlack) {
+
+      nextKlack = timeNow + 60000/slider.value;
+
+      beatCount++;
+      beatCount = beatCount % sliderBeats.value;
+
+      myBeats.textContent = beatCount + 1;
+
+      // console.log(sliderBeats.value)
+
+  }
 }
