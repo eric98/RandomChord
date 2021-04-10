cb1d643db392cbbd9a04bf20143992adb65b4ddb
Eric <ergare.17@gmail.com>
2021-04-10 04:11:18 +0200

    fifth progression working

--- main.js
+++ main.js
@@ -53,6 +53,9 @@ var nextKlack = 0;
 var myBeats = document.getElementById("myBeats");
 var beatCount = 0;
 
+var fifthsProgression = document.getElementById("_fifthProgression");
+fifthsProgression.checked = true;
+
 var lastElt = document.getElementById("_lastChord");
 var actualElt = document.getElementById("_actualChord");
 var nextElt = document.getElementById("_nextChord");
@@ -61,7 +64,38 @@ var lastChord = new Chord(lastElt, lastImage);
 var actualChord = new Chord(actualElt, actualImage);
 var nextChord = new Chord(nextElt, nextImage);
 
-function randomTonality() {
+var majChord = document.getElementById("_maj7");
+var dominantChord = document.getElementById("_dom");
+var minorChord = document.getElementById("_min7");
+var semiDismChord = document.getElementById("_semiDism7");
+var augChord = document.getElementById("_aug7");
+var minorMaj7Chord = document.getElementById("_minMaj7");
+
+majChord.checked = true;
+dominantChord.checked = false;
+minorChord.checked = false;
+semiDismChord.checked = false;
+augChord.checked = false;
+minorMaj7Chord.checked = false;
+
+chordStep();
+chordStep();
+chordStep();
+
+function chordStep() {
+
+  lastChord.setChord(actualChord);
+  actualChord.setChord(nextChord);
+  
+  if (fifthsProgression.checked) {
+    nextChord.setNextFifthChord();
+  }
+  else {
+    nextChord.setRandomChord();
+  }
+
+  lastChord.draw();
+  actualChord.draw();
   nextChord.draw();
 }
 
@@ -85,7 +119,7 @@ function tick() {
       myBeats.textContent = beatCount + 1;
 
       if (beatCount == 0) {
-        randomTonality();
+        chordStep();
 
         // if (plingActived) {
         //     // pling.play();
@@ -99,4 +133,99 @@ function tick() {
     }
 
   }
+}
+
+function checkMaj7() {
+  if (!isThereAnyChordSelected()) {
+    majChord.checked = true;
+  }
+}
+
+function check7() {
+  if (!isThereAnyChordSelected()) {
+    dominantChord.checked = true;
+  }
+}
+
+function checkMin7() {
+  if (!isThereAnyChordSelected()) {
+    minorChord.checked = true;
+  }
+}
+
+function checkSemiDism() {
+  if (!isThereAnyChordSelected()) {
+    semiDismChord.checked = true;
+  }
+}
+
+function checkAug7() {
+  if (!isThereAnyChordSelected()) {
+    augChord.checked = true;
+  }
+}
+
+function checkMinorMaj7() {
+  if (!isThereAnyChordSelected()) {
+    minorMaj7Chord.checked = true;
+  }
+}
+
+function isThereAnyChordSelected() {
+  return (
+      majChord.checked ||
+      dominantChord.checked ||
+      minorChord.checked ||
+      semiDismChord.checked ||
+      augChord.checked ||
+      minorMaj7Chord.checked
+    );
+}
+
+function canSetMode(_mode) {
+
+  switch(_mode) {
+      case 0:
+          return majChord.checked;
+      case 1:
+          return dominantChord.checked;
+      case 2:
+          return minorChord.checked;
+      case 3:
+          return semiDismChord.checked;
+      case 4:
+          return augChord.checked;
+      case 5:
+          return minorMaj7Chord.checked;
+  }
+}
+
+function getSemitone(tone, alter) {
+
+  let result = 0;
+  switch (tone) {
+      case 'A':
+          result = 0;
+          break;
+      case 'B':
+          result = 2;
+          break;
+      case 'C':
+          result = 3;
+          break;
+      case 'D':
+          result = 5;
+          break;
+      case 'E':
+          result = 7;
+          break;
+      case 'F':
+          result = 8;
+          break;
+      case 'G':
+          result = 10;
+          break;
+  }
+
+  return result + alter;
 }
--- model/Chord.js
+++ model/Chord.js
@@ -1,10 +1,224 @@
+const notes = "ABCDEFG"; 
+
 class Chord {
     constructor(_elt, _imgElt) {
         this.elt = _elt;
         this.imgElt = _imgElt;
-        this.name = "C";
-        this.alteration = "";
-        this.mode = "7♯5";
+
+        this.name = "D";
+        this.alteration = "♭";
+        this.mode = "Δ";
+    }
+
+    setMaj7() {
+        this.mode = 'Δ';
+    }
+
+    setDominant() {
+        this.mode = '7';
+    }
+
+    setMinor() {
+        this.mode = '-7';
+    }
+
+    setSemiDism() {
+        this.mode = 'ø';
+    }
+
+    setAug() {
+        this.mode = '7♯5';
+    }
+
+    setMinorMaj7() {
+        this.mode = '-Δ';
+    }
+
+    setRandomMode() {
+        
+        if (isThereAnyChordSelected()) {
+
+            let randChord;
+            do {
+                randChord = Math.floor(Math.random() * 6)
+            }
+            while(!canSetMode(randChord));
+
+            switch(randChord) {
+                case 0:
+                    this.setMaj7();
+                    break;
+                case 1:
+                    this.setDominant();
+                    break;
+                case 2:
+                    this.setMinor();
+                    break;
+                case 3:
+                    this.setSemiDism();
+                    break;
+                case 4:
+                    this.setAug();
+                    break;
+                case 5:
+                    this.setMinorMaj7();
+                    break;
+            }
+
+        }
+    }
+
+    setNote(_name, _alteration) {
+        if (_name == 'C' && _alteration == 1) {
+            this.name = 'D';
+            this.alteration = "♭"; 
+            return;
+        }
+
+        if (_name == 'D' && _alteration == 1) {
+            this.name = 'E';
+            this.alteration = "♭"; 
+            return;
+        }
+
+        if (_name == 'G' && _alteration == -1) {
+            this.name = 'F';
+            this.alteration = "♯"; 
+            return;
+        }
+
+        if (_name == 'G' && _alteration == 1) {
+            this.name = 'A';
+            this.alteration = "♭"; 
+            return;
+        }
+
+        if (_name == 'A' && _alteration == 1) {
+            this.name = 'B';
+            this.alteration = "♭"; 
+            return;
+        }
+
+        // semitones E - F && B - C
+        if (_name == 'E' && _alteration == 1) {
+            this.name = 'F';
+            this.alteration = ""; 
+            return;
+        }
+        if (_name == 'F' && _alteration == -1) {
+            this.name = 'E';
+            this.alteration = ""; 
+            return;
+        }
+        if (_name == 'B' && _alteration == 1) {
+            this.name = 'C';
+            this.alteration = ""; 
+            return;
+        }
+        if (_name == 'C' && _alteration == -1) {
+            this.name = 'B';
+            this.alteration = ""; 
+            return;
+        }
+
+        this.name = _name;
+        if (_alteration == 1) {
+            this.alteration = "♯"; 
+        }
+        else if (_alteration == 0) {
+            this.alteration = ""; 
+        }
+        else if (_alteration == -1) {
+            this.alteration = "♭"; 
+        }
+    }
+
+    setRandomNote() {
+        this.setNote(
+            notes.charAt(Math.floor(Math.random() * notes.length)),
+            Math.floor(Math.random() * 3) - 1
+        );
+    }
+
+    setChord(_chord) {
+        this.name = _chord.name;
+        this.alteration = _chord.alteration;
+        this.mode = _chord.mode;
+    }
+
+    setRandomChord() {
+        this.setRandomNote();
+        this.setRandomMode();
+    }
+
+    setNextFifthChord() {
+        let alterationNum = 0;
+        if (this.alteration == "♭") {
+            alterationNum = -1;
+        }
+        else if (this.alteration == "♯") {
+            alterationNum = 1;
+        }
+
+        let desiredSemitone = getSemitone(this.name, alterationNum) + 5;
+        desiredSemitone = desiredSemitone % 12;
+
+        let nextName;
+        let nextAlteration;
+
+        switch (desiredSemitone) {
+            case 0:
+                nextName = 'A';
+                nextAlteration = 0;
+                break;
+            case 1:
+                nextName = 'B';
+                nextAlteration = -1;
+                break;
+            case 2:
+                nextName = 'B';
+                nextAlteration = 0;
+                break;
+            case 3:
+                nextName = 'C';
+                nextAlteration = 0;
+                break;
+            case 4:
+                nextName = 'C';
+                nextAlteration = 1;
+                break;
+            case 5:
+                nextName = 'D';
+                nextAlteration = 0;
+                break;
+            case 6:
+                nextName = 'E';
+                nextAlteration = -1;
+                break;
+            case 7:
+                nextName = 'E';
+                nextAlteration = 0;
+                break;
+            case 8:
+                nextName = 'F';
+                nextAlteration = 0;
+                break;
+            case 9:
+                nextName = 'F';
+                nextAlteration = 1;
+                break;
+            case 10:
+                nextName = 'G';
+                nextAlteration = 0;
+                break;
+            case 11:
+                nextName = 'G';
+                nextAlteration = 1;
+                break;
+        }
+
+        this.setNote(nextName, nextAlteration);
+
     }
 
     draw() {
@@ -38,7 +252,27 @@ class Chord {
             auxChord = "mmaj7";
         }
 
-        let imgChordName = this.name + "/" + this.name + auxAlteration + auxChord + ".gif";
+        let prevName;
+        let nextName;
+
+        if (this.name == "E" && this.alteration == "♭") {
+            prevName = "D";
+            nextName = "DisE";
+        }
+        else if (this.name == "A" && this.alteration == "♭") {
+            prevName = "G";
+            nextName = "GisA";
+        }
+        else if (this.name == "B" && this.alteration == "♭") {
+            prevName = "A";
+            nextName = "AisB";
+        }
+        else {
+            prevName = this.name;
+            nextName = this.name;
+        }
+
+        let imgChordName = prevName + "/" + nextName + auxAlteration + auxChord + ".gif";
         this.imgElt.src = "https://www.musiklehre.at/all_piano_chords/img/"+imgChordName;
     }
 }
